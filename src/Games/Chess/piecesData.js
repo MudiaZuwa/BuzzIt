28016f770b1855cfc1de4c4c52a956993121b1b5
unknown <mudiaosazuwa@gmail.com>
2024-11-12 03:08:42 +0100

    chess pawn fix

--- src/Games/Chess/piecesData.js
+++ src/Games/Chess/piecesData.js
@@ -27,6 +27,6 @@ pieceImages.Player1.Rook.src = "/assets/Chess/b_rook_1x.png";
 pieceImages.Player2.Bishop.src = "/assets/Chess/w_bishop_1x.png";
 pieceImages.Player2.King.src = "/assets/Chess/w_king_1x.png";
 pieceImages.Player2.Knight.src = "/assets/Chess//w_knight_1x.png";
-pieceImages.Player2.Pawn.src = "/assets/Chess/w_Pawn_1x.png";
+pieceImages.Player2.Pawn.src = "/assets/Chess/w_pawn_1x.png";
 pieceImages.Player2.Queen.src = "/assets/Chess/w_queen_1x.png";
 pieceImages.Player2.Rook.src = "/assets/Chess/w_rook_1x.png";
