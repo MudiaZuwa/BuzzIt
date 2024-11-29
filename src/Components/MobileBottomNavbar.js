41b65d740898daeac6c100fda41fba38c2baadab
unknown <mudiaosazuwa@gmail.com>
2024-11-29 04:27:03 +0100

    multiple call modal fix

--- src/Components/MobileBottomNavbar.js
+++ src/Components/MobileBottomNavbar.js
@@ -1,18 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { off, getDatabase, ref, onChildAdded } from "../Components/firebase";
-import FetchDataFromNode from "../Functions/FetchDataFromNode";
-import AudioCallModal from "./AudioCallModal";
-import VideoCallModal from "./VideoCallModal";
 import AuthModal from "../Auth/AuthModal";
 import signOutUser from "../Functions/SignOutUser";
 
 const MobileBottomNavbar = ({ uid }) => {
-  const [callerId, setCallerId] = useState(null);
-  const [callType, setCallType] = useState(null);
-  const [showVideoCallModal, setShowVideoCallModal] = useState(false);
-  const [showAudioCallModal, setShowAudioCallModal] = useState(false);
   const [showProfileEditModal, setShowProfileEditModal] = useState(false);
   const [showAuthModal, setShowAuthModal] = useState(false);
 
@@ -24,76 +16,8 @@ const MobileBottomNavbar = ({ uid }) => {
     else handleAuthModalOpen();
   };
 
-  const handleVideoCallModalOpen = () => {
-    if (!showAudioCallModal && !showVideoCallModal) setShowVideoCallModal(true);
-  };
-  const handleVideoCallModalClose = () => setShowVideoCallModal(false);
-
-  const handleAudioCallModalOpen = () => {
-    if (!showAudioCallModal && !showVideoCallModal) setShowAudioCallModal(true);
-  };
-  const handleAudioCallModalClose = () => setShowAudioCallModal(false);
-
   const handleProfileEditModalOpen = () => {
     if (!showProfileEditModal) setShowProfileEditModal(true);
-  };
-  const handleProfileEditModalClose = () => setShowProfileEditModal(false);
-
-  useEffect(() => {
-    if (uid) checkUserDetails();
-  }, [uid]);
-
-  useEffect(() => {
-    if (!showVideoCallModal) {
-      setCallerId(null);
-      setCallType(null);
-    }
-  }, [showVideoCallModal]);
-
-  useEffect(() => {
-    if (callerId) {
-      // if (callType === "video") handleVideoCallModalOpen();
-      // else if (callType === "audio") handleAudioCallModalOpen();
-    }
-  }, [callerId, callType]);
-
-  const checkUserDetails = () => {
-    const nodePath = `UsersDetails/${uid}`;
-    FetchDataFromNode(nodePath)
-      .then((data) => {
-        if (!data) return;
-        else if (!data.name) handleProfileEditModalOpen();
-        if (data) {
-          ListenForCalls();
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
-
-  const ListenForCalls = () => {
-    const nodePath = `UsersCalls/${uid}`;
-    const database = getDatabase();
-    const nodeRef = ref(database, nodePath);
-    const unsubscribe = onChildAdded(nodeRef, (snapshot) => {
-      if (snapshot?.exists()) {
-        const call = snapshot.val();
-        if (
-          call.caller === "incoming" &&
-          !showAudioCallModal &&
-          !showVideoCallModal
-        ) {
-          setCallerId(call.friend);
-          setCallType(call.callType);
-        }
-      } else {
-        console.log("No data available at this node.");
-      }
-    });
-
-    // Return a function to stop listening
-    return () => off(nodeRef, "child_added", unsubscribe);
   };
 
   return (
@@ -146,20 +70,7 @@ const MobileBottomNavbar = ({ uid }) => {
           </Nav.Link>
         </Nav>
       </Container>
-      <VideoCallModal
-        show={showVideoCallModal}
-        handleClose={handleVideoCallModalClose}
-        uid={uid}
-        userId={callerId}
-        caller={"reciepient"}
-      />
-      <AudioCallModal
-        show={showAudioCallModal}
-        handleClose={handleAudioCallModalClose}
-        uid={uid}
-        userId={callerId}
-        caller={"reciepient"}
-      />
+
       <AuthModal
         show={showAuthModal}
         handleClose={handleAuthModalClose}
